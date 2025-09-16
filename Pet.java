ee444d6e61a46d78e802a7c85f6e576243aba987
lucasrk0606 <lucasrk0606@gmail.com>
2025-09-16 16:10:20 -0300

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -2,6 +2,10 @@ public class Pet {
     private String nomePet;
     private String tipoPet;
     public Pet(String nomePet, String tipoPet){
+        if(nomePet == null){
+            System.out.println("Cadastro do pet encerrado!");
+            System.exit(1); 
+        }
         this.nomePet=nomePet;
         this.tipoPet=tipoPet;
     }
